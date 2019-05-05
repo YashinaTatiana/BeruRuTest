95efd696dbe70e61432f1a90a1529f0fa711e631
Яшина Татьяна <kulikT97@mail.ru>
2019-05-05 20:10:37 +0300

    update

--- TestBeruRu/src/main/java/utils/Parameters.java
+++ TestBeruRu/src/main/java/utils/Parameters.java
@@ -2,6 +2,7 @@ package utils;
 
 public class Parameters {
 	
+	public static final String URL = "https://beru.ru/";
 	public static final String LOGIN = "testBeruRu@yandex.ru";
 	public static final String PASSWORD = "kulikandlake";
 	public static final String PRODUCT = "Электрические зубные щетки";
